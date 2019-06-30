e5d38f10b33ec13d7b04c55105652f58d29fb344
Jaid <jaid.jsx@gmail.com>
2019-07-01 01:29:40 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
 /** @module sitemap-xml-webpack-plugin */
 
-import path from "path"
-
-import publishimo from "publishimo"
-import {ConcatSource} from "webpack-sources"
-import {isString} from "lodash"
-import {AsyncParallelHook} from "tapable"
-import fss from "@absolunet/fss"
-import json5 from "json5"
-
-import generateBanner from "./generateBanner"
-import formatBanner from "./formatBanner"
+import resolveAny from "resolve-any"
+import sitemapBuilder from "sitemap"
+import {ensureObject} from "magina"
+import {sortBy} from "lodash"
 
 const webpackId = "SitemapXmlWebpackPlugin"
-const pkgHook = "publishimoGeneratedPkg"
 
 /**
  * @typedef pluginOptions
- * @type {object}
- * @property {function} [publishimo] Publishimo instance, just in case you want to override the required publishimo package with a newer or manually patched version
- * @property {string} [filename="package.json"] Output file name
- * @property {boolean|number} [format=false] If true, formats JSON output and indents for readability. If a number is given, it will be the indent width in spaces. If `true`, the indent with is `2`.
- * @property {boolean} [autoMain=true] If true, automatically set `main` field. If a string is given, it will be the key instead of `"main"`.
- * @property {boolean} [autoTypes=false] If true, automatically set `types` field.
- * @property {boolean} [banner=true] If true, add license banner to ouput script.
- * @property {boolean} [unicodeCopyright=true] If true, uses `©` instead of `(c)` in banner.
- * @property {boolean} [productionOnly=true] If true, only applies changes and emits files in Webpack mode `production`.
- * @property {string} [debugFolder=null] Directory where debug info files get written to.
- * @property {boolean} [json5=false] If true, package output will be written with `json5.stringify` instead of `JSON.stringify`.
- * @property {boolean} [autoExclude=false] If `true`, unneeded fields will be guessed and automatically excluded from output package.
- * @property {string[]} [includeFields=[]] Field names that should forcefully be forwarded from `options.pkg` to generated pkg. For example, use `includeFields: ["babel"]` to include your Babel config in your output package.
- * @property {string[]} [excludeFields=[]] Fields names that are never written to generated pkg.
+ * @type {Object}
+ * @prop {boolean} [productionOnly=true] If true, only applies changes and emits files in Webpack mode `production`.
+ * @prop {string} [fileName=sitemap.xml]
+ * @prop {string} domain
+ * @prop {string} [protocol=https]
+ * @prop {string[]} [paths=[]]
+ * @prop {string} [changeFrequency=daily] One of: `hourly`, `daily`, `weekly`, `monthly`
+ * @prop {boolean} [setLastMod=true]
+ * @prop {boolean} [sortPaths=true]
  */
 
 /**
@@ -43,100 +30,52 @@ export default class {
    * @param {pluginOptions} [options] Plugin options
    */
   constructor(options) {
+    const optionsObject = ensureObject(options, "domain")
     this.options = {
-      publishimo,
-      filename: "package.json",
-      format: false,
-      autoMain: true,
-      autoTypes: false,
-      banner: true,
-      unicodeCopyright: true,
       productionOnly: true,
-      debugFolder: null,
-      incudeFields: [],
-      excludeFields: [],
-      json5: false,
-      autoExclude: false,
-      ...options,
-    }
-    if (this.options.format === true) {
-      this.options.format = 2
-    }
-  }
-
-  outputDebugFile(filename, data) {
-    if (!this.options.debugFolder) {
-      return
-    }
-    const outputPath = path.join(this.options.debugFolder, filename)
-    if (typeof data === "string") {
-      fss.outputFile(outputPath, data)
-    } else {
-      fss.outputJson5(outputPath, data, {space: 2})
+      fileName: "sitemap.xml",
+      protocol: "https",
+      paths: [],
+      changeFrequency: "daily",
+      setLastMod: true,
+      sortPaths: true,
+      ...optionsObject,
     }
   }
 
   apply(compiler) {
+    if (!this.options.domain) {
+      throw new Error(`Option domain is required for ${_PKG_TITLE}`)
+    }
     if (this.options.productionOnly && compiler.options.mode !== "production") {
       return
     }
-    if (compiler.hooks[pkgHook]) {
-      throw new Error(`Webpack hook compiler.hooks.${pkgHook} is already registered`)
-    }
-    compiler.hooks[pkgHook] = new AsyncParallelHook(["publishimoResult"])
-    let publishimoResult
-    compiler.hooks.compilation.tap(webpackId, compilation => {
-      compilation.hooks.optimizeChunkAssets.tapPromise(webpackId, async chunks => {
-        const publishimoConfig = {
-          pkg: compiler.context,
-          ...this.options,
-        }
-        const chunkPath = path.join(compilation.outputOptions.path, compilation.chunks[0].files[0])
-        const mainPath = path.relative(compilation.outputOptions.path, chunkPath)
-        if (this.options.autoMain) {
-          const fieldKey = isString(this.options.autoMain) ? this.options.autoMain : "main"
-          publishimoConfig[fieldKey] = mainPath
-        }
-        if (this.options.autoTypes) {
-          publishimoConfig.types = mainPath
-        }
-        if (this.options.autoExclude && compilation.options.target !== "node") { // Don't include dependencies in package.json if export target is not a Node module
-          publishimoConfig.excludeFields = [
-            ...publishimoConfig.excludeFields,
-            "dependencies",
-            "peerDependencies",
-            "optionalDependencies",
-          ]
-        }
-        publishimoResult = await this.options.publishimo(publishimoConfig)
-        this.outputDebugFile("options.json5", this.options)
-        this.outputDebugFile("publishimoResult.json5", publishimoResult)
-        compiler.hooks[pkgHook].promise(publishimoResult)
-        if (this.options.banner) {
-          const banner = do {
-            if (this.options.banner === true) {
-              generateBanner(publishimoResult.generatedPkg, this.options)
-            } else if (typeof this.options.banner === "function") {
-              this.options.banner(publishimoResult.generatedPkg)
-            } else if (typeof this.options.banner === "string") {
-              this.options.banner
-            } else {
-              "?"
-            }
+    const lastmodISO = new Date().toISOString()
+    compiler.hooks.emit.tapPromise(webpackId, async compilation => {
+      const paths = [
+        "",
+        ...await resolveAny(this.options.paths),
+      ]
+      const urls = paths
+        |> #.map(path => {
+          const entry = ensureObject(path, "url")
+          if (!entry.lastmodISO && this.options.setLastMod) {
+            entry.lastmodISO = lastmodISO
           }
-          const finalBanner = formatBanner(banner)
-          this.outputDebugFile("banner.js", finalBanner)
-          for (const chunk of chunks) {
-            for (const file of chunk.files) {
-              compilation.assets[file] = new ConcatSource(finalBanner, "\n", compilation.assets[file])
-            }
+          if (!entry.changefreq && this.options.changeFrequency) {
+            entry.changefreq = this.options.changeFrequency
           }
-        }
+          return entry
+        })
+        |> sortBy(#, "url")
+      const sitemap = sitemapBuilder.createSitemap({
+        urls,
+        cacheTime: 0,
+        hostname: `${this.options.protocol}://${this.options.domain}`,
       })
-    })
-    compiler.hooks.emit.tap(webpackId, compilation => {
-      const fileContents = (this.options.json5 ? json5 : JSON).stringify(publishimoResult.generatedPkg, null, this.options.format || 0)
-      compilation.assets[this.options.filename] = {
+      const fileContents = sitemap.toString()
+      const fileName = await resolveAny(this.options.fileName)
+      compilation.assets[fileName] = {
         source: () => fileContents,
         size: () => fileContents.length,
       }
---
+++ test/test.js
@@ -0,0 +1,58 @@
+import path from "path"
+
+import webpack from "webpack"
+import pify from "pify"
+import {CleanWebpackPlugin} from "clean-webpack-plugin"
+import ms from "ms.macro"
+import fsp from "@absolunet/fsp"
+import {last} from "lodash"
+
+const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
+const {default: SitemapXmlWebpackPlugin} = indexModule
+
+jest.setTimeout(ms`1 minute`)
+
+const getWepbackConfig = name => ({
+  mode: "production",
+  context: path.join(__dirname, name),
+  entry: path.join(__dirname, name),
+  output: {
+    path: path.join(__dirname, "..", "dist", "test", name),
+  },
+})
+
+it("should run", async () => {
+  const name = "basic"
+  const webpackConfig = {
+    ...getWepbackConfig(name),
+    plugins: [
+      new CleanWebpackPlugin,
+      new SitemapXmlWebpackPlugin("example.com"),
+    ],
+  }
+  await pify(webpack)(webpackConfig)
+  const xml = await fsp.readXml(path.join(__dirname, "..", "dist", "test", name, "sitemap.xml"))
+  expect(xml.urlset.$.xmlns).toMatch("sitemaps.org/")
+  expect(xml.urlset.url[0]).toMatchObject({
+    loc: ["https://example.com"],
+    changefreq: ["daily"],
+  })
+})
+
+it("should run with custom urls", async () => {
+  const name = "customUrls"
+  const webpackConfig = {
+    ...getWepbackConfig(name),
+    plugins: [
+      new CleanWebpackPlugin,
+      new SitemapXmlWebpackPlugin({
+        domain: "example.com",
+        paths: ["c", "a", "a/b"],
+      }),
+    ],
+  }
+  await pify(webpack)(webpackConfig)
+  const xml = await fsp.readXml(path.join(__dirname, "..", "dist", "test", name, "sitemap.xml"))
+  expect(xml.urlset.url.length).toBe(4)
+  expect(last(xml.urlset.url).loc[0]).toBe("https://example.com/c")
+})
